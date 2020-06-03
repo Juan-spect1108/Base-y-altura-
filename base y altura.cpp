c142ffc1576e2ea95a71a199375ffcf05a72a00d
Juan Guzman <63362277+Juan-spect1108@users.noreply.github.com>
2020-06-02 22:11:17 -0500

    Add files via upload

---
+++ base y altura.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+
+int altura=0;
+int base=0;
+
+
+cout<<"Designe la altutra: ";cin>>altura;
+cout<<"Designe la base: ";cin>>base;
+
+int resultado= (altura*base);
+cout<<resultado;
+}
